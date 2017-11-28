65d6e00a2745b2b13324a750cc89b7377f4f46fe
Dilpreet1995 <31404611+Dilpreet1995@users.noreply.github.com>
2017-11-28 17:06:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
+const router2=jsonServer.router('user.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
